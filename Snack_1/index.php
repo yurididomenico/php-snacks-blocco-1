b13720ba5bf1f7e2f2e4874cc8644ebf4cd99086
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-13 14:37:09 +0100

    PHP: Stampa di prova array partite

--- Snack_1/index.php
+++ Snack_1/index.php
@@ -10,12 +10,37 @@ Esempio:
 Olimpia Milano - Cantù | 55-60 
 
 -->
-
+<!-- ----- PHP ------------------------------------------------------------------------------------------------- -->
 <?php
 
-    
+    $array = 
+    [
+        [
+            "squadraCasa" => "Licantropi",
+            "puntiSquadraCasa" => 20,
+            "squadraOspite" => "Vampiri",
+            "puntiSquadraOspite" => 30,
+        ],
+
+        [
+            "squadraCasa" => "Maghi",
+            "puntiSquadraCasa" => 43,
+            "squadraOspite" => "Babbani",
+            "puntiSquadraOspite" => 15,
+        ],
+
+        [
+            "squadraCasa" => "Ladri",
+            "puntiSquadraCasa" => 23,
+            "squadraOspite" => "Poliziotti",
+            "puntiSquadraOspite" => 34,
+        ]
+    ];
+
 ?>
 
+
+<!-- ----- HTML ------------------------------------------------------------------------------------------------- -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,15 +50,49 @@ Olimpia Milano - Cantù | 55-60
     <title>YDD - Snack 1</title>
 </head>
 <body>
-    
+    <div class="contenitore">
+        <ul>
+            <?php            
+                for($i=0; $i<count($array); $i++)
+                {
+                    echo "<li>";
+                        echo $array[$i]["squadraCasa"]." ";
+                        echo $array[$i]["puntiSquadraCasa"]."<br>";
+
+                        echo $array[$i]["squadraOspite"]." ";
+                        echo $array[$i]["puntiSquadraOspite"]."<br>";
+                    echo "</li>";
+                }
+            ?>
+        </ul>
+    </div>
 </body>
 </html>
 
+
+<!-- ----- STILE ------------------------------------------------------------------------------------------------- -->
 <style>
 
-    body
-    {
-        background-color: #c6c6c6;
-    }
+*
+{
+padding: 0;
+margin: 0;
+box-sizing: border-box;
+color: inherit;
+text-decoration: none;
+list-style-type: none;
+transition-duration: 200ms;
+}
+
+body
+{
+    background-color: #c6c6c6;
+    padding: 50px;
+}
+
+.contenitore ul li
+{
+    padding: 10px 0;
+}
 
 </style>
