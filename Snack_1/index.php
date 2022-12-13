555a40e422f2963aa1705b2e70c667883d54ab53
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-13 14:05:11 +0100

    Set Workspace

---
+++ Snack_1/index.php
@@ -0,0 +1,39 @@
+<!--
+//Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+-Ogni array avrà:
+    -una squadra di casa e una squadra ospite
+    -punti fatti dalla squadra di casa e punti fatti dalla squadra ospite
+-Stampiamo a schermo tutte le partite con questo schema.
+
+Esempio:
+Olimpia Milano - Cantù | 55-60 
+
+-->
+
+<?php
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YDD - Snack 1</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<style>
+
+    body
+    {
+        background-color: #c6c6c6;
+    }
+
+</style>
