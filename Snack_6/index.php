32eeb1a94bf7ecef584d835cbdf78e4b554881d4
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-13 18:36:46 +0100

    Set Workspace

---
+++ Snack_6/index.php
@@ -0,0 +1,73 @@
+<!-- 
+Snack 6
+Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+ -->
+<!-- ----- PHP ------------------------------------------------------------------------------------------------- -->
+<?php
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+
+?>
+
+
+<!-- ----- HTML ------------------------------------------------------------------------------------------------- -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YDD - Snack 6</title>
+</head>
+<body>
+    <div class="contenitore">
+        
+        
+    </div>
+</body>
+</html>
+
+
+<!-- ----- STILE ------------------------------------------------------------------------------------------------- -->
+<style>
+
+*
+{
+padding: 0;
+margin: 0;
+box-sizing: border-box;
+color: inherit;
+text-decoration: none;
+list-style-type: none;
+transition-duration: 200ms;
+}
+
+body
+{
+    background-color: #c6c6c6;
+    padding: 50px;
+}
+
+</style>
