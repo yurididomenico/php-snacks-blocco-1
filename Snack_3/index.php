18e8bc095b9395c17abddd55b3c31bee9161dbdd
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-13 16:36:35 +0100

    PHP: Stampa date

--- Snack_3/index.php
+++ Snack_3/index.php
@@ -1,23 +1,61 @@
 <!--
-Snack 2
--Passare come parametri GET:
-    -name
-    -mail
-    -age
--Verificare se:
-    -name sia più lungo di 3 caratteri
-    -mail contenga un punto e una chiocciola
-    -age sia un numero.
--Controllare e stampare:
-    -“Accesso riuscito” se l'if risulta true
-    -"Accesso negato” se l'if risulta false
+Snack 3
+Creare un array di array.
+Ogni array figlio avrà come chiave una data in questo formato:
+    -DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
 
 -->
 <!-- ----- PHP ------------------------------------------------------------------------------------------------- -->
 <?php
 
-   
-    
+    $posts = 
+    [
+
+        '10-01-2019' => 
+        [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+
+        '10-02-2019' => 
+        [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+
+        '15-05-2019' => 
+        [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+
+
 ?>
 
 
@@ -32,7 +70,16 @@ Snack 2
 </head>
 <body>
     <div class="contenitore">
+        <?php
         
+        // echo key($posts);
+
+        foreach(array_keys($posts) as $key){
+            echo $key;
+        }
+            
+        
+        ?>
     </div>
 </body>
 </html>
