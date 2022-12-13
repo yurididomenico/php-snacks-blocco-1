8240cb8454fd3ecfe10fa2ebd86be1f9810092bf
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2022-12-13 15:54:32 +0100

    Set Workspace

---
+++ Snack_3/index.php
@@ -0,0 +1,63 @@
+<!--
+Snack 2
+-Passare come parametri GET:
+    -name
+    -mail
+    -age
+-Verificare se:
+    -name sia più lungo di 3 caratteri
+    -mail contenga un punto e una chiocciola
+    -age sia un numero.
+-Controllare e stampare:
+    -“Accesso riuscito” se l'if risulta true
+    -"Accesso negato” se l'if risulta false
+
+-->
+<!-- ----- PHP ------------------------------------------------------------------------------------------------- -->
+<?php
+
+   
+    
+?>
+
+
+<!-- ----- HTML ------------------------------------------------------------------------------------------------- -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YDD - Snack 3</title>
+</head>
+<body>
+    <div class="contenitore">
+        
+    </div>
+</body>
+</html>
+
+
+<!-- ----- STILE ------------------------------------------------------------------------------------------------- -->
+<style>
+
+*
+{
+padding: 0;
+margin: 0;
+box-sizing: border-box;
+color: inherit;
+text-decoration: none;
+list-style-type: none;
+transition-duration: 200ms;
+}
+
+body
+{
+    background-color: #c6c6c6;
+    padding: 50px;
+}
+
+
+
+</style>
